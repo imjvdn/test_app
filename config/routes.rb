d48bbc7e11f88995901add950f107845185f81de
jadanmorrow <jadanmorrow@outlook.com>
2023-08-27 20:27:54 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # This is the route for the articles controller
+  resources :articles
+end
