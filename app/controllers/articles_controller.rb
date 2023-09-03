db022e6b88496e36db31de0213eaa83b9f92f35e
jadanmorrow <jadanmorrow@outlook.com>
2023-09-03 08:48:52 -0700

    safe place

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
+  # Include the authentication check
+  # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  # Existing before_action to set the article
   before_action :set_article, only: %i[show edit update destroy]
 
-  # GET /articles or /articles.json
+  # ... (rest of your code)
+
+  # Existing index method
   def index
     if params[:search]
       @articles = Article.where("title LIKE ?", "%#{params[:search]}%")
@@ -10,37 +15,30 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # GET /articles/1 or /articles/1.json
+  # Existing show method
   def show
-    # No additional logic needed for showing an article
   end
 
-  # GET /articles/new
+  # Existing new method
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
+  # Existing edit method
   def edit
-    # No additional logic needed for editing an article
   end
 
-  # POST /articles or /articles.json
+  # Existing create method
   def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to article_url(@article), notice: "Article was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /articles/1 or /articles/1.json
+  # Existing update method
   def update
     respond_to do |format|
       if @article.update(article_params)
@@ -53,10 +51,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1 or /articles/1.json
+  # Existing destroy method
   def destroy
     @article.destroy
-
     respond_to do |format|
       format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
       format.json { head :no_content }
@@ -65,12 +62,11 @@ class ArticlesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+  # Existing private methods
   def set_article
     @article = Article.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def article_params
     params.require(:article).permit(:title, :description)
   end
